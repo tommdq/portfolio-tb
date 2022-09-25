2143cec356527c7ca106e3a7c345c4019493ad97
tommdq <registom123@gmail.com>
2022-09-25 14:19:08 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,astro}"],
+    theme: {
+        extend: {
+            colors: {
+                "primary-green": "#34a958"
+            }
+        }
+    },
+    plugins: []
+};
