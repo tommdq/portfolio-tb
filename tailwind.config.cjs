7a8d518d80ba9c49dcfa41dd3319dbb35152a558
tommdq <registom123@gmail.com>
2022-10-07 23:59:55 -0300

    Palette color change & removed svg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,astro}"],
-    theme: {
-        extend: {
-            colors: {
-                "primary-green": "#34a958"
-            }
-        }
-    },
-    plugins: []
-};
+	content: ['./src/**/*.{html,js,astro}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary-green': '#34a958',
+				'hover-blue': '#48CAE4'
+			}
+		}
+	},
+	plugins: []
+}
