b580c434a5e24da56f36976ad339829388d92ebb
tommdq <registom123@gmail.com>
2022-10-10 16:52:52 -0300

    new design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,26 @@ module.exports = {
 				softBlue: '#467599',
 				softGreen: '#e6e2f1',
 				hoverGreen: '#9ED8DB'
+			},
+			keyframes: {
+				wiggle: {
+					'0%': { transform: 'rotate(0deg)' },
+					'30%': { transform: 'rotate(1080deg)' },
+					'50%': { transform: 'rotate(1080deg)' },
+					'55%': { transform: 'rotate(1092deg)' },
+					'60%': { transform: 'rotate(1072deg)' },
+					'65%': { transform: 'rotate(1094deg)' },
+					'70%': { transform: 'rotate(1076deg)' },
+					'72%': { transform: 'rotate(1080deg)' },
+					'80%': { transform: 'rotate(1080deg)' },
+					'85%': { transform: 'rotate(1092deg)' },
+					'90%': { transform: 'rotate(1072deg)' },
+					'95%': { transform: 'rotate(1094deg)' },
+					'100%': { transform: 'rotate(1076deg)' }
+				}
+			},
+			animation: {
+				compass: 'wiggle 2s linear'
 			}
 		}
 	},
