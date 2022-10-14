b22d5133976168a6f4a6ba4fdd91e99a42dbf12e
Tomas Buzeta <registom123@gmail.com>
2022-10-14 19:45:39 -0300

    updated dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,12 +13,16 @@ module.exports = {
 				navBlue: '#21395b',
 				softBlue: '#467599',
 				softGreen: '#e6e2f1',
+				star: 'linear-gradient(90deg, #fff, transparent)'
 			},
 			width: {
 				navIcon: '2.5rem'
 			},
 			height: {
 				navIcon: '2.5rem'
+			},
+			boxShadow: {
+				star: '0 0 0 4px rgba(255, 255, 255, 0.1), 0 0 0 8px rgba(255,255,255,0.1), 0 0 0 20px rgba(255,255,255,1)'
 			},
 			keyframes: {
 				wiggle: {
@@ -35,15 +39,40 @@ module.exports = {
 					'90%': { transform: 'rotate(1072deg)' },
 					'95%': { transform: 'rotate(1094deg)' },
 					'100%': { transform: 'rotate(1076deg)' }
+				},
+				animateBg: {
+					'0%,100%': { transform: 'scale(1)' },
+					'50%': { transform: 'scale(1.2)' }
+				},
+				animateStar: {
+					'0%': { transform: 'rotate(315deg) translateX(0)' },
+					'70%': { opacity: 1},
+					'100%': { transform: 'rotate(315deg) translateX(-1000px)', opacity: 0 }
 				}
 			},
 			animation: {
 				compass: 'wiggle 2s linear',
+				stars: 'animateStar 2s linear infinite'
+			},
+			animationDuration: {
+				1250: '1250ms',
+				1500: '1500ms',
+				1750: '1750ms',
+				2500: '2500ms',
+				2000: '2000ms',
+				2250: '2250ms',
+				3000: '3000ms'
+			},
+			animationDelay: {
+				2750: '2750ms',
 			},
 			screens: {
 				md: '860px'
 			}
 		}
 	},
-	plugins: []
+	plugins: [
+		require('tailwindcss-animation-delay'),
+		require('tailwindcss-animation')
+	]
 }
