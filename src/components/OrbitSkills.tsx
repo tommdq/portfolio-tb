eeecb8c34a7c81fdb153b4adea5d015a10f0ecc3
Tomas Buzeta <registom123@gmail.com>
2022-10-30 23:55:31 -0300

    icons added

--- src/components/OrbitSkills.tsx
+++ src/components/OrbitSkills.tsx
@@ -1,3 +1,5 @@
+import { ICONS } from '../constants/icons'
+
 const OrbitSkills = () => {
 	return (
 		<ul className='grid h-[40rem] w-[40rem] place-items-center'>
@@ -20,46 +22,46 @@ const OrbitSkills = () => {
 			</li>
 
 			<li className='orbit-ring h-[23rem] w-[23rem] '>
+				<img src={ICONS.JIRA} alt='jira' className='orbit-icon -top-4 right-1/2 ' />
 				<img
-					src='../../assets/skills/react.svg'
-					alt='typescript'
-					className='orbit-icon -top-4 right-1/2 '
-				/>
-				<img
-					src='../../assets/skills/git.svg'
+					src={ICONS.GIT}
 					alt='git'
 					className='orbit-icon -bottom-4 right-1/2 rounded-full'
 				/>
-				<img src='' alt='' />
-				<img src='' alt='' />
+				<img src={ICONS.VIM} alt='vim' className='orbit-icon -right-4 top-1/2 ' />
+				<img src={ICONS.REACT} alt='react' className='orbit-icon -left-4 top-1/2' />
+				<img
+					src={ICONS.VERCEL}
+					alt='vercel'
+					className='orbit-icon left-6 top-12 rounded-full bg-slate-300'
+				/>
+				<img
+					src={ICONS.NPM}
+					alt='npm'
+					className='orbit-icon left-6 bottom-12 rounded-full bg-slate-300'
+				/>
+				<img
+					src={ICONS.TAILWIND}
+					alt='tailwind'
+					className='orbit-icon right-6 bottom-12'
+				/>
+				<img
+					src={ICONS.TESTING_LIBRARY}
+					alt='testing library'
+					className='orbit-icon roundedfull right-6 top-12'
+				/>
 			</li>
 
 			<li className='orbit-ring h-[15rem] w-[15rem] '>
+				<img src={ICONS.HTML} alt='html5' className='orbit-icon top-16 -left-2 ' />
+				<img src={ICONS.CSS} alt='css' className='orbit-icon bottom-0 left-8' />
+				<img src={ICONS.JS} alt='js' className='orbit-icon top-12 right-0' />
 				<img
-					src='../../assets/skills/html5.svg'
-					alt='html5'
-					className='orbit-icon top-16 -left-2 '
-				/>
-				<img
-					src='../../assets/skills/css.svg'
-					alt='css'
-					className='orbit-icon bottom-0 left-8'
-				/>
-				<img
-					src='../../assets/skills/js.svg'
-					alt='js'
-					className='orbit-icon top-12 right-0'
-				/>
-				<img
-					src='../../assets/skills/typescript.svg'
+					src={ICONS.TYPESCRIPT}
 					alt='typescript'
 					className='orbit-icon bottom-4 right-6'
 				/>
-				<img
-					src='../../assets/skills/node.svg'
-					alt='node'
-					className='orbit-icon -top-4 left-24'
-				/>
+				<img src={ICONS.NODE} alt='node' className='orbit-icon -top-4 left-24' />
 			</li>
 		</ul>
 	)
---
+++ src/constants/icons.tsx
@@ -0,0 +1,38 @@
+import html from '../../assets/skills/html5.svg'
+import css from '../../assets/skills/css.svg'
+import git from '../../assets/skills/git.svg'
+import jira from '../../assets/skills/jira.svg'
+import js from '../../assets/skills/js.svg'
+import node from '../../assets/skills/node.svg'
+import react from '../../assets/skills/react.svg'
+import typescript from '../../assets/skills/typescript.svg'
+import vim from '../../assets/skills/vim.svg'
+import npm from '../../assets/skills/npm.svg'
+import vercel from '../../assets/skills/vercel.svg'
+import tailwind from '../../assets/skills/tailwind.svg'
+import astro from '../../assets/skills/astro.svg'
+import testingLibrary from '../../assets/skills/testinglibrary.svg'
+// TODO: sin usar
+import jest from '../../assets/skills/jest.svg'
+import fresh from '../../assets/skills/fresh.svg'
+import vite from '../../assets/skills/vitejs.svg'
+
+export const ICONS = {
+  HTML: html,
+  CSS: css,
+  JS: js,
+  NODE: node,
+  TYPESCRIPT: typescript,
+  REACT: react,
+  VIM: vim,
+  GIT: git,
+  JIRA: jira,
+  VERCEL: vercel,
+  NPM: npm,
+  TAILWIND: tailwind,
+  ASTRO: astro,
+  TESTING_LIBRARY: testingLibrary,
+  JEST: jest,
+  FRESH: fresh,
+  VITE: vite
+}
